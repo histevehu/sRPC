b83bdb69b79e968289680934461218c69439c5ee
SteveHu <histevehu@outlook.com>
2024-05-03 00:17:50 +0800

    使用ThreadPoolFactory创建Socket服务端的线程池

--- sRPC-core/src/main/java/top/histevehu/srpc/core/socket/server/SocketServer.java
+++ sRPC-core/src/main/java/top/histevehu/srpc/core/socket/server/SocketServer.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import top.histevehu.srpc.common.enumeration.RpcError;
 import top.histevehu.srpc.common.exception.RpcException;
+import top.histevehu.srpc.common.util.ThreadPoolFactory;
 import top.histevehu.srpc.core.RpcServer;
 import top.histevehu.srpc.core.registry.ServiceRegistry;
 import top.histevehu.srpc.core.serializer.CommonSerializer;
@@ -18,10 +19,6 @@ import java.util.concurrent.*;
  */
 public class SocketServer implements RpcServer {
 
-    private static final int CORE_POOL_SIZE = 5;
-    private static final int MAXIMUM_POOL_SIZE = 50;
-    private static final int KEEP_ALIVE_TIME = 60;
-    private static final int BLOCKING_QUEUE_CAPACITY = 100;
     private final ExecutorService threadPool;
     private final ServiceRegistry serviceRegistry;
     private CommonSerializer serializer;
@@ -34,9 +31,7 @@ public class SocketServer implements RpcServer {
      */
     public SocketServer(ServiceRegistry serviceRegistry) {
         this.serviceRegistry = serviceRegistry;
-        BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<>(BLOCKING_QUEUE_CAPACITY);
-        ThreadFactory threadFactory = Executors.defaultThreadFactory();
-        threadPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, workingQueue, threadFactory);
+        threadPool = ThreadPoolFactory.createDefaultThreadPool("sRPC-SocketServer");
     }
 
     public void start(int port) {
